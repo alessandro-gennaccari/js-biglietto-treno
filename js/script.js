f06337014f638eed50c6102f5f354ef880c590c5
alessandro-gennaccari <alessandrogennaccari@gmail.com>
2020-12-14 16:00:48 +0100

    added results in html

--- js/script.js
+++ js/script.js
@@ -15,10 +15,17 @@ var oneKmPrice = 0.21; // Dichiaro variabile con il prezzo di ogni singolo km;
 var totalPrice = travelDistance * oneKmPrice; // Calcolo il prezzo del biglietto senza sconto;
 var discount = 0; // Dichiaro la variabile sconto, con valore di partenza 0;
 
+document.getElementById('km').innerHTML = 'Devi percorrere:' + travelDistance + 'km ed ';
+document.getElementById('age').innerHTML = 'Hai ' + passengerAge + ' anni, quindi ';
+document.getElementById('price').innerHTML = 'Pagheresti: ' + totalPrice + ' € ';
+document.getElementById('discount').innerHTML = 'Non hai diritto a sconti';
+
 if (passengerAge < 18) {
     discount = (totalPrice * 20 / 100); // Calcolo lo sconto per minorenni;
-    totalPrice -= discount; 
+    totalPrice -= discount;
+    document.getElementById('discount').innerHTML = 'Hai diritto ad uno sconto, quindi pagherai: ' + totalPrice + ' €';
 } else if (passengerAge >= 65) {
     discount = (totalPrice * 40 / 100); // Calcolo lo sconto per gli over65;
     totalPrice -= discount;
+    document.getElementById('discount').innerHTML = 'Hai diritto ad uno sconto, quindi pagherai: ' + totalPrice + ' €';
 }
