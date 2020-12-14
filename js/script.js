794d702b92ad561c210eef61da27b29c85acbb67
alessandro-gennaccari <alessandrogennaccari@gmail.com>
2020-12-14 13:36:17 +0100

    init

---
+++ js/script.js
@@ -0,0 +1,11 @@
+/* Chiedere all’utente:
+- il numero di chilometri che vuole percorrere,
+- l’età del passeggero.
+
+- calcolare il prezzo totale del viaggio.
+Il prezzo del biglietto è definito in base
+ai km (0.21 € al km):
+
+- sconto del 20% per i minorenni
+- sconto del 40% per gli over 65. */
+
